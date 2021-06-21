789a320d28b433f29af818397f345022ae4b5025
lucas <lucas.videcoq1@gmail.com>
2021-06-21 23:27:08 +0200

    initial commit: done:  -help command  -connection to a channel  -.env file to store token

--- src/main/java/fr/jaggernaute/jarvis/Listener.java
+++ src/main/java/fr/jaggernaute/jarvis/Listener.java
@@ -1,11 +1,17 @@
 package fr.jaggernaute.jarvis;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import java.awt.*;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
 
 
 public class Listener extends ListenerAdapter {
@@ -20,7 +26,41 @@ public class Listener extends ListenerAdapter {
             String content = message.getContentRaw();
             if (content.equals("!ping"))
             {
-                channel.sendMessage("Pong!").queue();
+                channel.sendMessage("Fuck !").queue();
+            }else if (content.equals("!help"))
+            {
+                EmbedBuilder eb = new EmbedBuilder();
+
+                eb.setTitle("Jarvis help center", null);
+
+                eb.setColor(Color.blue);
+                eb.setColor(new Color(0xC13F3F));
+                eb.setColor(new Color(184, 38, 66));
+
+                eb.setDescription("Command list:");
+                eb.addField("!ping", "return : Pong! (for debug purposes only)", true);
+                eb.addField("!help", "Show this panel", true);
+
+                eb.addBlankField(false);
+                eb.setAuthor("Help", null, "https://imgur.com/mN81ttG.png");
+
+                LocalDateTime localDate = LocalDateTime.now();
+                DateTimeFormatter myFormatDay = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                String formattedDate = localDate.format(myFormatDay);
+                DateTimeFormatter myFormatTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+                String formattedTime = localDate.format(myFormatTime);
+                eb.setFooter("Sent by Jarvis the " + formattedDate + " at " + formattedTime);
+
+                eb.setImage("https://i.imgur.com/aJajRjp.png");
+                eb.setThumbnail("https://imgur.com/iWdqJ0S.png");
+                channel.sendMessage(eb.build())
+                        .queue();
+            }else if(content.equals("!music")){
+
+                Guild guild = event.getGuild();
+                VoiceChannel voiceChannel = guild.getVoiceChannelsByName("vocal", true).get(0);
+                AudioManager audioManager = guild.getAudioManager();
+                audioManager.openAudioConnection(voiceChannel);
             }
         }
     }
--- src/main/java/fr/jaggernaute/jarvis/Main.java
+++ src/main/java/fr/jaggernaute/jarvis/Main.java
@@ -5,17 +5,32 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import javax.security.auth.login.LoginException;
+import java.util.Map;
 
 
 public class Main
 {
     public static void main(String[] arguments) throws LoginException, InterruptedException {
 
-        String token = "NzY0NTQyNjM1NTYxODQ0NzM2.X4Hx0A.eBo2YSk5pyZd2lzQ8MY0z3C3Bsw";
+        System.out.println("   __                   _             ___ _                 \n" +
+                "   \\ \\  __ _ _ ____   _(_)___        / __\\ |__   __ _ _ __  \n" +
+                "    \\ \\/ _` | '__\\ \\ / / / __|_____ / /  | '_ \\ / _` | '_ \\ \n" +
+                " /\\_/ / (_| | |   \\ V /| \\__ \\_____/ /___| | | | (_| | | | |\n" +
+                " \\___/ \\__,_|_|    \\_/ |_|___/     \\____/|_| |_|\\__,_|_| |_|\n" +
+                "                                                            ");
 
-        JDA api = JDABuilder.createDefault(token).addEventListeners(new Listener()).build().awaitReady();
+        Dotenv dotenv = Dotenv.configure().load();
+        String token = dotenv.get("TOKEN");
+
+        JDA api = JDABuilder.createDefault(token)
+                .addEventListeners(new Listener())
+                .build()
+                .awaitReady();
+
+        System.out.println("\033[0;32m"+"Jarvis-Chan is online masta !");
     }
 }
 

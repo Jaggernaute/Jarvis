85bf0a0262e4261048fe6a5c7c95b6734fd3bdaa
lucas <lucas.videcoq1@gmail.com>
2021-07-08 16:10:40 +0200

    initial commit: done: - Embed builder - 404
    to do:
    -help

--- src/main/java/fr/jaggernaute/jarvis/Listener.java
+++ src/main/java/fr/jaggernaute/jarvis/Listener.java
@@ -6,22 +6,30 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import static java.lang.Integer.parseInt;
+
 public class Listener extends ListenerAdapter
 {
     private final char prefix = '!';
     MainMusic mainMusic = new MainMusic();
+    EmbedFormater embedFormater = new EmbedFormater();
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] splitCommand = event.getMessage().getContentRaw().split("^!", 2);
         if (event.getAuthor().isBot()) return;
+        if(event.getMessage().getContentRaw().isEmpty())return;
         if(event.getMessage().getContentRaw().charAt(0) == prefix) {
             String[] splitargs = splitCommand[1].split(" ", 2);
             switch (splitargs[0]) {
                 case "ping" -> pong(event.getChannel(), event);
                 case "my" -> cabbages(event.getChannel());
                 case "pig" -> pig(event.getChannel());
+                case "purge" -> purge(event.getChannel(), parseInt(splitargs[1]));
                 case "play" -> mainMusic.loadAndPlay(event.getChannel(), splitargs[1]);
+                case "help" -> help(event.getChannel());
+
+                default -> cmdNotFound(event.getChannel());
             }
         }else return;
 
@@ -44,5 +52,33 @@ public class Listener extends ListenerAdapter
         String pig = ":pig:";
         String result = EmojiParser.parseToUnicode(pig);
         channel.sendMessage(result).queue();
+    }
+
+    public void purge(TextChannel channel, int amount) {
+        channel.getIterableHistory()
+                .takeAsync(amount)
+                .thenAccept(channel::purgeMessages);
+
+        channel.sendMessage("Purging channel!")
+                .queue(response-> {
+                    response.editMessageFormat(amount + " messages have been purges").queue();
+                });
+    }
+
+    public void help(TextChannel channel){
+        channel.sendMessage("Fuck U bitch ! I use java ! ☕").queue();
+    }
+
+    public void cmdNotFound(TextChannel channel){
+        channel.sendMessage(embedFormater.embedFormater("This comand doesn't exist",
+                "The command you are trying to call is not implemented yet",
+                "For help :",
+                "``!help``",
+                null,
+                null,
+                "Author: Jaggernaute",
+                "https://emoji.gg/assets/emoji/8649_FoxxoTail.gif",
+                "https://emoji.gg/assets/emoji/5620_FoxxoReally.png",
+                null)).queue();
     }
 }
